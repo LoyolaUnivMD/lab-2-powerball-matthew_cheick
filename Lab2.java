3e816dde064333e53bf84f5f31fe24483dc6f564
matthewwoodside <mrwoodside@loyola.edu>
2024-01-31 09:59:28 -0500

    Lab 2 Java File

--- Lab2.java
+++ Lab2.java
@@ -1,27 +1,29 @@
-// Programmers:  [your names here]
-// Course:  CS 212
-// Due Date:
-// Lab Assignment:
-// Problem Statement:
-// Data In:
-// Data Out:
-// Credits: [Is your code based on an example in the book, in class, or something else?
-//            Reminder: you should never take code from the Internet or another person
-
 import java.util.Scanner;
+import java.util.Random;
+import java.text.DecimalFormat;
 
 class Lab2 {
     public static void main(String[] args) {
         long prize = 225938745L;
         Scanner input = new Scanner(System.in);
-
         System.out.println("CS 212 - Lab 2");
-        System.out.println("This program generates 10 lottery tickets.");
-        
-        System.out.print("What's your name? ");
-
-        String customerName = input.nextLine();
-
+        System.out.println("Welcome to CS212 live betting, the only program where students under 21 operate the lottery system. This game will allow you to receive 10 lottery tickets and, ultimately, see the prize pool. Good luck, and if you have a gambling problem seek professional help!");
+        System.out.println("What's your name? ");
+        String player = input.nextLine();
+        System.out.println("Here are the tickets: ");
+        Random number = new Random();
+        DecimalFormat fmt = new DecimalFormat("#00");
+        int i = 0;
+        while (i < 10) {
+            System.out.println(fmt.format(number.nextInt(99)) + " " + fmt.format(number.nextInt(99)) + " " +fmt.format(number.nextInt(99)) + " " +fmt.format(number.nextInt(99)) + " " +fmt.format(number.nextInt(99)) + " " +fmt.format(number.nextInt(99)));
+            i ++;
+            }
+        System.out.println("------------------");
+        System.out.println("Good luck " + player.strip() + "!");
+        System.out.println("Estimated Jackpot: ");
+        DecimalFormat form = new DecimalFormat("#000,000,000");
+        System.out.println("$" + form.format(prize));
+        System.out.println("------------------");
+        }
 
     }
-}
